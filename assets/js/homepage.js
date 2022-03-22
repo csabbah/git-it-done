526fc167b7cda883a6254d16d290c7a6a4d72db3
Csabbah <csabbah@ryerson.ca>
2022-03-22 11:30:41 -0400

    Added a functioning form field that returns user github repositories and checks if they have any github issues

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -3,11 +3,83 @@ var getUserRepos = function (user) {
   var apiUrl = `https://api.github.com/users/${user}/repos`;
 
   // make a request to the url
-  fetch(apiUrl).then(function (response) {
-    response.json().then(function (data) {
-      console.log(data);
+  fetch(apiUrl)
+    .then(function (response) {
+      // request was successful
+      if (response.ok) {
+        response.json().then(function (data) {
+          displayRepos(data, user);
+        });
+      } else {
+        alert('Error: GitHub User Not Found');
+      }
+    })
+    .catch(function (error) {
+      // Notice this `.catch()` getting chained onto the end of the `.then()` method
+      alert('Unable to connect to GitHub');
     });
+};
+
+function formSubmitHandler(e) {
+  e.preventDefault();
+  if (nameInputEl.value.trim()) {
+    getUserRepos(nameInputEl.value.trim()); //  Get rid of empty spaces
+    nameInputEl.value = '';
+  } else {
+    alert('Please fill in the form');
+  }
+}
+
+const userFormEl = document.getElementById('user-form');
+var nameInputEl = document.getElementById('username');
+userFormEl.addEventListener('submit', formSubmitHandler);
+
+var displayRepos = function (repos, searchTerm) {
+  // check if api returned any repos
+  if (repos.length === 0) {
+    repoContainerEl.textContent = 'No repositories found.';
+    return;
+  }
+
+  console.log(repos);
+  console.log(searchTerm);
+  // clear old content
+  repoContainerEl.textContent = '';
+  repoSearchTerm.textContent = searchTerm;
+
+  repos.forEach((item) => {
+    var repoEl = document.createElement('div');
+    repoEl.classList = 'list-item flex-row justify-space-between align-center';
+
+    // create a span element to hold repository name
+    var titleEl = document.createElement('span');
+    titleEl.textContent = `${searchTerm}/${item.name}`;
+
+    // append to container
+    repoEl.appendChild(titleEl);
+
+    // create a status element
+    var statusEl = document.createElement('span');
+    statusEl.classList = 'flex-row align-center';
+
+    // check if current repo has issues or not
+    if (item.open_issues_count > 0) {
+      statusEl.innerHTML =
+        "<i class='fas fa-times status-icon icon-danger'></i>" +
+        item.open_issues_count +
+        ' issue(s)';
+    } else {
+      statusEl.innerHTML =
+        "<i class='fas fa-check-square status-icon icon-success'></i>";
+    }
+
+    // append to container
+    repoEl.appendChild(statusEl);
+
+    // append container to the dom
+    repoContainerEl.appendChild(repoEl);
   });
 };
 
-getUserRepos('microsoft');
+var repoContainerEl = document.querySelector('#repos-container');
+var repoSearchTerm = document.querySelector('#repo-search-term');
